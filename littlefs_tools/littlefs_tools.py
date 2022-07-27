a79937933e26982391e01d4db86aa76a71e8c72a
Vysakh P Pillai <3634378+vppillai@users.noreply.github.com>
2022-07-27 11:56:06 -0700

    Update littlefs_tools.py

--- littlefs_tools/littlefs_tools.py
+++ littlefs_tools/littlefs_tools.py
@@ -4,46 +4,24 @@ import argparse
 from pathlib import Path
 from littlefs import LittleFS
 import logging
+from colorama import init
+from colorama import Fore, Back, Style
+init(autoreset=True)
+
+
+logger = logging.getLogger("littlefs_tools")
+
+def set_log_level(verbose):
+    logging.basicConfig(format='%(levelname)s - %(lineno)d - %(message)s')
+    if(verbose):
+        print("setting logger level to debug")
+        logger.setLevel(logging.DEBUG)
+    else:
+        logger.setLevel(logging.WARNING)
+
 
 def remove_prefix(text, prefix):
     return text[text.startswith(prefix) and len(prefix):]
-
-def globPath(args):
-    try:
-        fs = LittleFS(block_size=args.blockSize, block_count=args.blockCount)
-        contentSize=0
-        for path, subdirs, files in os.walk(args.source):
-            for subdir in subdirs:
-                lfsDir="/"+remove_prefix(path,args.source).strip("/")
-                if("/"!=lfsDir):
-                    fs.mkdir(lfsDir)
-                logging.info(f"LFS Folder : {lfsDir}")
-            for name in files:
-                filePath=os.path.join(path, name)
-                lfsPath=os.path.join(lfsDir, name)
-                with open(filePath, 'rb') as file:        
-                    fileSize=os.path.getsize(filePath)
-                    contentSize+=fileSize
-                    if contentSize > (args.blockSize * (args.blockCount-2)):
-                        logging.critical("Contents wont fit into the disk. Plese adjust block size and block count")
-                        logging.critical(f"\tTotal Contents size = {contentSize} Bytes")
-                        logging.critical(f"\tImage size [{args.blockSize} x {args.blockCount}] = {args.blockSize*args.blockCount} Bytes")
-                        exit(-1)
-                    with fs.open(lfsPath, 'wb') as fh:
-                        logging.info(f"LFS File   : {lfsPath} ({fileSize} Bytes)")        
-                        byte = file.read(1)
-                        while byte:
-                            fh.write(byte)
-                            byte = file.read(1)
-        print(f"\nTotal Contents size = {contentSize} Bytes")
-
-        # Dump the filesystem content to a file
-        with open(args.image, 'wb') as fh:
-            fh.write(fs.context.buffer)
-    
-    except Exception as e:
-        logging.critical(e)
-        exit(-2)
 
 def sizeof_fmt(num, suffix="B"):
     for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
@@ -57,12 +35,14 @@ def _print_tree(fs,path):
         depth=path.count("/")-1
         fsContent=fs.scandir(path)      
         for item in fsContent:
+            logger.debug(f"{item.name} {item.type}")
             if item.type==2:
+                print(f"  {depth*'    '}{Fore.CYAN}{item.name}")
                 _print_tree(fs,f"{path}/{item.name}")
             else:
-                print(f"  {depth*'    '}├──{item.name} {('',f'({sizeof_fmt(item.size)})')[item.type==1]}")
+                print(f"  {depth*'    '}└──{item.name} {('',f'({sizeof_fmt(item.size)})')[item.type==1]}")
     except Exception as e:
-        logging.critical(e)
+        logger.critical(e)
         exit(-2)
 
 def _lsfiles(args):
@@ -71,11 +51,11 @@ def _lsfiles(args):
         with open(args.image, 'rb') as fh:
             fs.context.buffer = bytearray(fh.read())
         fs.mount()
-        print(f"{args.image}")
+        print(f"{Fore.GREEN}{args.image}")
         _print_tree(fs,"/")
 
     except Exception as e:
-        logging.critical(e)
+        logger.critical(e)
         exit(-2)
 
 def list_files():
@@ -92,11 +72,7 @@ def list_files():
                                     help='image file name', required=True)
     args = parser.parse_args()
 
-    logging.basicConfig(format='%(levelname)s - %(lineno)d - %(message)s')
-    if(args.verbose):
-        logging.basicConfig(level=logging.INFO)
-    else:
-        logging.basicConfig(level=logging.WARNING)
+    set_log_level(args.verbose)
     
     _lsfiles(args)
 
@@ -104,23 +80,23 @@ def _walk_fs_tree(fs,path,destination):
     try:
         fsContent=fs.scandir(path)      
         for item in fsContent:
+            logger.debug(f"{item.name} {item.type}")
             if item.type==1: #type=File
                 with fs.open(f"{path}/{item.name}", 'rb') as fh:
                     with open (f"{destination}/{path}/{item.name}", 'wb') as f:
                         f.write(fh.read())
             if item.type==2: #type=Folder
-                if not os.path.exists(os.path.join(destination,item.name)):
-                    os.makedirs(os.path.join(destination,item.name),exist_ok=True)
+                Path(f"{destination}/{path}/{item.name}").mkdir(parents=True,exist_ok=True)
                 _walk_fs_tree(fs,f"{path}/{item.name}",destination)
     except Exception as e:
-        logging.critical(e)
+        logger.critical(e)
         exit(-2)
 
 def _extract_files(args):
     try:
         #check if args.destination exists and is an empty folder
         if not args.force and os.path.exists(args.destination) and os.listdir(args.destination):
-            logging.critical(f"{args.destination} is not an empty folder")
+            logger.critical(f"{args.destination} is not an empty folder")
             exit(-1)
 
         fs = LittleFS(block_size=args.blockSize, block_count=args.blockCount,mount=False)
@@ -131,7 +107,7 @@ def _extract_files(args):
         _walk_fs_tree(fs,"/",args.destination)
         print(f"Extracted files to {args.destination}")
     except Exception as e:
-        logging.critical(e)
+        logger.critical(e)
         exit(-2)
 
 def extract_files():
@@ -151,15 +127,50 @@ def extract_files():
                                help='destination directory to extract the contents into',required=True)
     args = parser.parse_args()
 
-    logging.basicConfig(format='%(levelname)s - %(lineno)d - %(message)s')
-    if(args.verbose):
-        logging.basicConfig(level=logging.INFO)
-    else:
-        logging.basicConfig(level=logging.WARNING)
+    set_log_level(args.verbose)
     
     _extract_files(args)
 
-def main_entry():
+def globPath(args):
+    try:
+        fs = LittleFS(block_size=args.blockSize, block_count=args.blockCount)
+        contentSize=0
+        for path, subdirs, files in os.walk(args.source):
+            for subdir in subdirs:
+                lfsDir=os.path.join("/",remove_prefix(path,args.source),subdir)
+                if("/"!=lfsDir):
+                    fs.mkdir(lfsDir)
+                    logger.info(f"LFS Directory : {lfsDir}")
+            
+            for name in files:
+                filePath=os.path.join(path, name)
+                lfsPath=os.path.join('/', remove_prefix(filePath,args.source))
+
+                with open(filePath, 'rb') as file:
+                    fileSize=os.path.getsize(filePath)
+                    contentSize+=fileSize
+                    if contentSize > (args.blockSize * (args.blockCount-2)):
+                        logger.critical("Contents wont fit into the disk. Plese adjust block size and block count")
+                        logger.critical(f"\tTotal Contents size = {contentSize} Bytes")
+                        logger.critical(f"\tImage size [{args.blockSize} x {args.blockCount}] = {args.blockSize*args.blockCount} Bytes")
+                        exit(-1)
+                    with fs.open(lfsPath, 'wb') as fh:
+                        logger.info(f"LFS File   : {lfsPath} ({fileSize} Bytes)")
+                        byte = file.read(1)
+                        while byte:
+                            fh.write(byte)
+                            byte = file.read(1)
+        print(f"\nTotal Contents size = {contentSize} Bytes")
+
+        # Dump the filesystem content to a file
+        with open(args.image, 'wb') as fh:
+            fh.write(fs.context.buffer)
+
+    except Exception as e:
+        logger.critical(e)
+        exit(-2)
+
+def create_image():
     parser = argparse.ArgumentParser(
         description=f"Tool to generate lfs images from a source folder", prog="littlefs_create")
     parser.add_argument('-b', '--block_size', dest='blockSize',
@@ -175,13 +186,12 @@ def main_entry():
                         help='source path',required=True)
     args = parser.parse_args()
 
-    logging.basicConfig(format='%(levelname)s - %(lineno)d - %(message)s')
-    if(args.verbose):
-        logging.basicConfig(level=logging.INFO)
-    else:
-        logging.basicConfig(level=logging.WARNING)
+    set_log_level(args.verbose)
     
     globPath(args)
 
     print(f"Created `{args.image}` of size [{args.blockSize} x {args.blockCount}] = {args.blockSize*args.blockCount} Bytes")
 
+
+if __name__ == "__main__":
+    list_files()
